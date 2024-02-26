fbd74bca56b40285f46df757159a8b6eacaf114a
Shubham Khandare <sk@Shubhams-MacBook-Pro.local>
2024-02-26 20:53:22 +0530

    updated csrf trusted origins

--- project/settings/base.py
+++ project/settings/base.py
@@ -6,6 +6,10 @@ from .env import ABS_PATH, ENV_BOOL, ENV_INT, ENV_LIST, ENV_STR
 DEBUG = ENV_BOOL("DEBUG", False)
 SECRET_KEY = "3d07dc7e-2dd1-4eb4-b5c3-f642d8e7d633"
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "https://movie-schedule.onrender.com"
+]
 # SECRET_KEY = ENV_STR("SECRET_KEY", "secret" if DEBUG else "")
 # ALLOWED_HOSTS = ENV_LIST("ALLOWED_HOSTS", ",", ["*"] if DEBUG else [])
 
